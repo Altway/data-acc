4c14eab96ca5aec3124859e180317ff0137c65b7
Altway <remi.gazquez@gmail.com>
2023-02-12 01:40:14 +0100

    Okex is now handled

--- trying.py
+++ trying.py
@@ -1,5 +1,7 @@
 import math
 import latexify
+import numpy as np
+from scipy.stats import norm
 
 @latexify.function
 def expected_value(k):
@@ -18,3 +20,81 @@ for i in range(11):
     print(f"{riskiness(1, i, 1):.2f}")
 
 expected_value
+
+
+N_prime = norm.pdf
+N = norm.cdf
+
+
+def black_scholes_call(S, K, T, r, sigma):
+    '''
+
+    :param S: Asset price
+    :param K: Strike price
+    :param T: Time to maturity
+    :param r: risk-free rate (treasury bills)
+    :param sigma: volatility
+    :return: call price
+    '''
+
+    ###standard black-scholes formula
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    call = S * N(d1) -  N(d2)* K * np.exp(-r * T)
+    return call
+
+def vega(S, K, T, r, sigma):
+    '''
+
+    :param S: Asset price
+    :param K: Strike price
+    :param T: Time to Maturity
+    :param r: risk-free rate (treasury bills)
+    :param sigma: volatility
+    :return: partial derivative w.r.t volatility
+    '''
+
+    ### calculating d1 from black scholes
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / sigma * np.sqrt(T)
+
+    #see hull derivatives chapter on greeks for reference
+    vega = S * N_prime(d1) * np.sqrt(T)
+    return vega
+
+
+
+def implied_volatility_call(C, S, K, T, r, tol=0.0001,
+                            max_iterations=100):
+    '''
+
+    :param C: Observed call price
+    :param S: Asset price
+    :param K: Strike Price
+    :param T: Time to Maturity
+    :param r: riskfree rate
+    :param tol: error tolerance in result
+    :param max_iterations: max iterations to update vol
+    :return: implied volatility in percent
+    '''
+
+
+    ### assigning initial volatility estimate for input in Newton_rap procedure
+    sigma = 0.3
+    
+    for i in range(max_iterations):
+
+        ### calculate difference between blackscholes price and market price with
+        ### iteratively updated volality estimate
+        diff = black_scholes_call(S, K, T, r, sigma) - C
+
+        ###break if difference is less than specified tolerance level
+        if abs(diff) < tol:
+            print(f'found on {i}th iteration')
+            print(f'difference is equal to {diff}')
+            break
+
+        ### use newton rapshon to update the estimate
+        sigma = sigma - diff / vega(S, K, T, r, sigma)
+
+    return sigma
